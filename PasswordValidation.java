96a2acce5799e45e07aff47f5fbfe7ca1d0bede4
Jason Kim <hoho4811@gmail.com>
2021-03-02 09:50:01 +1300

    credential 3

--- PasswordValidation.java
+++ PasswordValidation.java
@@ -1,8 +1,12 @@
 public class PasswordValidation {
 	public static void main(String[] args) {
 		
+
+
 		validatePwd validpwd = new validatePwd();
 		
+
+
 		String password = "sfsdffsdTsdf";
 		
 		if(validpwd.validationMethod(password)) {
