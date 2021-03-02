96366b26c575ef662f99b98e6220f19a1da3ed7c
Jason Kim <hoho4811@gmail.com>
2021-03-02 13:11:25 +1300

    TC commit

--- PasswordValidation.java
+++ PasswordValidation.java
@@ -1,14 +1,8 @@
 public class PasswordValidation {
 	public static void main(String[] args) {
-		
-
 
 		validatePwd validpwd = new validatePwd();
-		
-
-
-		String password = "sfsdffsdTsdf";
-		
+		String password = "sfsdffsdTsdf";		
 		if(validpwd.validationMethod(password)) {
 			System.out.println("Welcome to the AIS");
 		}else {
